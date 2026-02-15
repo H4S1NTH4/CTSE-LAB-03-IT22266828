02d0e25a34932681d3761e38fd54f5b43583351e
H4S1NTH4 <jmhdilsha@gmail.com>
2026-02-15 13:12:49 +0530

    Add Product model, repository, and controller with CRUD operations

---
+++ src/main/java/com/sliit/product_service/model/Product.java
@@ -0,0 +1,52 @@
+package com.sliit.product_service.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private Double price;
+
+    // Default constructor
+    public Product() {
+    }
+
+    // Constructor with parameters
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
